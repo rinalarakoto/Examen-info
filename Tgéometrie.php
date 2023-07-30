bae45e78fa1fac397347d6f675adc9199ef758c2
rinalarakoto <139998819+rinalarakoto@users.noreply.github.com>
2023-07-30 16:47:05 +0300

    Add files via upload

---
+++ Tgéometrie.php
@@ -0,0 +1,24 @@
+<?php
+if ($_SERVER['REQUEST_METHOD']=="POST"){
+try{
+   $connexion=new PDO("mysql:host=localhost;dbname=mathématique","root",""); 
+   $connexion->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+   echo "Bonjour ";
+echo $_POST["prenom"]."."."<br>";
+}
+catch(Exception $e){
+    echo "erreur " .$e->getMessage();
+}
+$titre=$connexion->prepare('SELECT * FROM géometrie WHERE titre=?');
+$titre->execute(array($_POST['titre']));
+if ($titre->rowCount()==0){
+  echo "on a pas encore cette livre";
+}
+  else {
+  while ($ligne=$titre->fetch()){
+    echo "Titre: " .$ligne["Titre"]. "  "."<br>"."écrit(e)(s) par: ".$ligne["Auteurs"]."  "."<br>"."Nombre de page: ".$ligne["Page"]."  "."<br>"."Langue: ".$ligne["Langue"]."  "."<br>"."Niveau: ".$ligne["Niveau"]."  "."<br>"."Prix en Euro: ".$ligne["Prix"]."  "."<br>";
+  
+  }
+}
+}
+?>
---
+++ géometrie.php
@@ -0,0 +1,40 @@
+<?php
+$serveur="localhost";
+$login="root";
+$pass="";
+try{
+$connexion=new PDO("mysql:host=$serveur;dbname=mathématique",$login,$pass);
+$connexion->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+$requete=$connexion->prepare("SELECT * FROM géometrie");
+$requete->execute();
+$resultat=$requete->fetchall();
+}
+catch(PDOException $e){
+    echo "echec".$e->getMessage();
+}
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>bibliothèque scientifique</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+    <form method="POST" action="Tgéometrie.php">
+    <fieldset id="s1">
+        <legend>Information personnelles :</legend>
+        <label for="prenom">Entrer votre prénom :</label>
+        <input type="text" id="prenom" name="prenom"><br>
+        <label for="nom">Entrer votre nom :</label>
+        <input type="text" id="nom" name="nom"><br>
+</fieldset>
+
+    <fieldset id="s2">
+        <legend>Information sur vos livre :</legend>
+        <label for="titre ">Entrer le titre de votre choix.</label>
+        <input type="text" id="titre" name="titre"><br>
+    </fieldset>
+        <input type="submit" value="Envoyer">
+    </body>
+</html>
